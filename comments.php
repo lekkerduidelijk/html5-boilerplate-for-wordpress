be0df6447a15eacfc6ab556a29e6d98fed57db09
RL <lekkerduidelijk@gmail.com>
2011-10-07 15:36:16 +0300

    Added translation methods

--- comments.php
+++ comments.php
@@ -9,7 +9,7 @@
 		die ('Please do not load this page directly. Thanks!');
 	
 	if ( post_password_required() ) { ?>
-		<p class="nocomments">This post is password protected. Enter the password to view comments.</p>
+		<p class="nocomments"><?php _e('This post is password protected. Enter the password to view comments','html5-boilerplate-for-wordpress') ?>.</p>
 		<?php
 		return;
 	}
@@ -40,9 +40,9 @@
 		
 	<?php else : // comments are closed ?>
 		
-		<!-- If comments are closed. -->
-		<p class="nocomments">Comments are closed.</p>
-		
+		<!-- If comments are closed. --> 
+		<p class="nocomments"><?php _e('Comments are closed','html5-boilerplate-for-wordpress') ?>.</p>
+
 	<?php endif; ?>
 <?php endif; ?>
 
@@ -50,42 +50,42 @@
 	
 	<section id="respond">
 		
-		<h3><?php comment_form_title( 'Leave a Reply', 'Leave a Reply to %s' ); ?></h3>
+		<h3><?php comment_form_title( __('Leave a Reply','html5-boilerplate-for-wordpress'), __('Leave a Reply to %s','html5-boilerplate-for-wordpress') ); ?></h3>
 		
 		<div class="cancel-comment-reply">
 			<small><?php cancel_comment_reply_link(); ?></small>
 		</div>
 		
 		<?php if ( get_option('comment_registration') && !is_user_logged_in() ) : ?>
-			<p>You must be <a href="<?php echo wp_login_url( get_permalink() ); ?>">logged in</a> to post a comment.</p>
+			<p><?php _e('You must be','html5-boilerplate-for-wordpress'); ?> <a href="<?php echo wp_login_url( get_permalink() ); ?>"><?php _e('logged in','html5-boilerplate-for-wordpress') ?></a> <?php _e('to post a comment','html5-boilerplate-for-wordpress') ?>.</p>
 		<?php else : ?>
 			<form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
 				
 				<?php if ( is_user_logged_in() ) : ?>
-					<p>Logged in as <a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a>. <a href="<?php echo wp_logout_url(get_permalink()); ?>" title="Log out of this account">Log out &raquo;</a></p>
+					<p><?php _e( 'Logged in as', 'html5-boilerplate-for-wordpress' ); ?> <a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a>. <a href="<?php echo wp_logout_url(get_permalink()); ?>" title="<?php _e('Log out of this account','html5-boilerplate-for-wordpress') ?>"><?php _e('Log out &raquo;','html5-boilerplate-for-wordpress') ?></a></p>
 				<?php else : ?>
 					<p>
-						<label for="author">Name <?php if ($req) echo "(required)"; ?></label>
+						<label for="author"><?php _e( 'Name', 'html5-boilerplate-for-wordpress' ); ?> <?php if ($req) echo __( '(required)', 'html5-boilerplate-for-wordpress' ); ?></label>
 						<input type="text" name="author" id="author" value="<?php echo esc_attr($comment_author); ?>" size="22" tabindex="1" <?php if ($req) echo "aria-required='true'"; ?> />
 					</p>
 					
 					<p>
-						<label for="email"><small>Mail (will not be published) <?php if ($req) echo "(required)"; ?></small></label>
+						<label for="email"><small><?php _e( 'Mail (will not be published)', 'html5-boilerplate-for-wordpress' ); ?> <?php if ($req) echo __( '(required)', 'html5-boilerplate-for-wordpress' ); ?></small></label>
 						<input type="email" name="email" id="email" value="<?php echo esc_attr($comment_author_email); ?>" size="22" tabindex="2" <?php if ($req) echo "aria-required='true'"; ?> />
 					</p>
 					
 					<p>
-						<label for="url">Website</label>
+						<label for="url"><?php _e( 'Website', 'html5-boilerplate-for-wordpress' ); ?></label>
 						<input type="url" name="url" id="url" value="<?php echo esc_attr($comment_author_url); ?>" size="22" tabindex="3" />
 					</p>
 				<?php endif; ?>
 				
-				<p id="allowed_tags"><strong>XHTML:</strong> You can use these tags: <code><?php echo allowed_tags(); ?></code></p>
+				<p id="allowed_tags"><strong>XHTML:</strong> <?php _e( 'You can use these tags', 'html5-boilerplate-for-wordpress' ); ?>: <code><?php echo allowed_tags(); ?></code></p>
 				
 				<p><textarea name="comment" id="comment" cols="58" rows="10" tabindex="4"></textarea></p>
 				
 				<p>
-					<input name="submit" type="submit" id="submit" tabindex="5" value="Submit Comment" />
+					<input name="submit" type="submit" id="submit" tabindex="5" value="<?php _e( 'Submit comment', 'html5-boilerplate-for-wordpress' ); ?>" />
 					<?php comment_id_fields(); ?>
 				</p>
 				<?php do_action('comment_form', $post->ID); ?>
